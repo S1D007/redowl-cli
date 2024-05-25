8c5b2d5674e57f47eac1ba5835632eb73a953dd4
Siddhant <s1d.official011001011@gmail.com>
2024-05-25 13:46:13 +0530

    fix(install): added npm and node install function using nvm

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ You can install the CLI tool using the following command:
 
 ```sh
 curl -sSL https://raw.githubusercontent.com/S1D007/redowl-cli/main/install.sh | bash
+
+```sh
+wget -qO- https://raw.githubusercontent.com/S1D007/redowl-cli/main/install.sh | bash
--- install.sh
+++ install.sh
@@ -7,6 +7,35 @@ INSTALL_DIR="$HOME/.redowl"
 BIN_DIR="$HOME/.local/bin"
 APP_NAME="redowl"
 
+
+check_npm_installed() {
+    if ! command -v npm &> /dev/null; then
+        echo "npm is not installed. Installing Node.js and npm using nvm..."
+        install_node_with_nvm
+    else
+        echo "npm is already installed."
+    fi
+}
+
+
+install_node_with_nvm() {
+
+    if ! command -v nvm &> /dev/null; then
+        echo "nvm is not installed. Installing nvm..."
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    fi
+
+    nvm install 20
+    nvm use 20
+    echo "Node.js and npm installed successfully, if you dont want to use nvm, you can uninstall it by running 'rm -rf $NVM_DIR'"
+}
+
+
+check_npm_installed
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     OS="linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -35,10 +64,11 @@ fi
 
 git clone "$REPO_URL" "$INSTALL_DIR"
 
+
 cd "$INSTALL_DIR" || exit
 
-$NPM_CMD install
 
+$NPM_CMD install
 
 if [ "$OS" == "windows" ]; then
     mkdir -p "$BIN_DIR"
