6e6c9f3f87b379572adbcc87f6e11b46a153cdab
Siddhant <s1d.official011001011@gmail.com>
2024-05-25 15:01:07 +0530

    fixed install script

--- install.sh
+++ install.sh
@@ -36,62 +36,9 @@ install_node_with_nvm() {
 
 check_npm_installed
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    OS="linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    OS="mac"
-elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
-    OS="windows"
-else
-    echo "Unsupported OS: $OSTYPE"
-    exit 1
-fi
-
-echo "Detected OS: $OS"
-
-if [ "$OS" == "windows" ]; then
-    INSTALL_DIR="$USERPROFILE\\.redowl"
-    BIN_DIR="$USERPROFILE\\.redowl\\bin"
-    NPM_CMD="npm.cmd"
-else
-    NPM_CMD="npm"
-    mkdir -p $BIN_DIR
-fi
-
-if [ -d "$INSTALL_DIR" ]; then
-    rm -rf "$INSTALL_DIR"
-fi
-
-git clone "$REPO_URL" "$INSTALL_DIR"
-
-
-cd "$INSTALL_DIR" || exit
-
-
-$NPM_CMD install
-
-if [ "$OS" == "windows" ]; then
-    mkdir -p "$BIN_DIR"
-    cp "$INSTALL_DIR/bin/$APP_NAME" "$BIN_DIR/$APP_NAME.cmd"
-    echo "Add $BIN_DIR to your PATH environment variable."
-else
-    ln -sf "$INSTALL_DIR/bin/$APP_NAME" "$BIN_DIR/$APP_NAME"
-
-    SHELL_NAME=$(basename "$SHELL")
-    if [[ "$SHELL_NAME" == "bash" ]]; then
-        PROFILE_FILE="$HOME/.bashrc"
-    elif [[ "$SHELL_NAME" == "zsh" ]]; then
-        PROFILE_FILE="$HOME/.zshrc"
-    else
-        echo "Unsupported shell: $SHELL"
-        exit 1
-    fi
-
-    if ! grep -qx 'export PATH="$HOME/.local/bin:$PATH"' "$PROFILE_FILE"; then
-        echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$PROFILE_FILE"
-    fi
-
-    source "$PROFILE_FILE"
-fi
+cd $INSTALL_DIR
+npm install
+npm i -g yarn
+(yarn unlink --global | true) && npm run build && yarn link --global
 
 echo "$APP_NAME installed successfully. You can run it using the command: $APP_NAME"
